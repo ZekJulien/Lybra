ad50c9ad158d9435f1dedef4bb37afd5f6e08380
Zek Julien <110969316+ZekJulien@users.noreply.github.com>
2025-09-06 23:08:30 +0200

    [FEAT] Add global toast notifications and update progress bar
    - Integrate PrimeNG MessageService and Toast component for global notifications
    - Refactor progress bar to be fixed at top of page and always visible during loading
    - Apply related style updates
    - Update app config

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,15 @@ import {Component, inject} from '@angular/core';
 import { RouterOutlet} from '@angular/router';
 import {LoadingService} from './core/services/loading.service';
 import { ProgressBarModule } from 'primeng/progressbar';
+import {Toast, ToastModule} from 'primeng/toast';
 
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    ProgressBarModule
+    ProgressBarModule,
+    Toast,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
