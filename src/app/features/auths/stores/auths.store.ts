6024de4764b00129e1feb9bcd65f4f90c3ced31b
Zek Julien <110969316+ZekJulien@users.noreply.github.com>
2025-09-06 15:16:10 +0200

    [REFACTOR] Implement async session restore and token refresh logic
    - Add async session restoration in AuthStore
    - Integrate async session restore in auth guard and HTTP interceptor for handling 401 errors and token refresh
    - Update login navigation logic
    - Introduce refresh method in AuthsService
    - Improve authentication flow and session management

--- src/app/features/auths/stores/auths.store.ts
+++ src/app/features/auths/stores/auths.store.ts
@@ -2,6 +2,8 @@ import {Injectable, signal, computed, effect, inject, Injector, runInInjectionCo
 import {Auth, Login} from '../models';
 import {AuthsService} from '../services';
 import {Router} from '@angular/router';
+import {AppRoutes} from '../../../app.routes';
+import {MainRoutes} from '../../../core/routes/main.routes';
 
 @Injectable({ providedIn: 'root' })
 export class AuthStore {
@@ -24,9 +26,37 @@ export class AuthStore {
         if (result) {
           this.auth.set(result);
           localStorage.setItem('refresh', result.refresh);
-          this.router.navigate(['']).then();
+          this.router.navigate([AppRoutes.MAIN, MainRoutes.DASHBOARD]).then();
         }
       }, { manualCleanup: true } );
+    });
+  }
+
+  restoreSessionAsync(): Promise<boolean> {
+    const refresh = localStorage.getItem('refresh');
+    if (!refresh) {
+      return Promise.resolve(false);
+    }
+
+    const signal = this.authsService.refresh(refresh);
+
+    return new Promise(resolve => {
+      runInInjectionContext(this.injector, () => {
+        const stop: EffectRef = effect(() => {
+          const result = signal();
+          if (result !== null) {
+            this.auth.set(result);
+            localStorage.setItem('refresh', result.refresh);
+            resolve(true);
+            stop.destroy();
+          }
+        });
+
+        setTimeout(() => {
+          stop.destroy();
+          resolve(false);
+        }, 3000);
+      });
     });
   }
 
